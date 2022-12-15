3dd44dcfd5ba53556e613a715dd95c64380124f4
ManuYaro <yaroemmanuelbekassoe@gmail.com>
2022-12-15 11:33:46 +0000

    6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "9800139a0eab4b8b7a1d24a6812b3a79",
-"/": "9800139a0eab4b8b7a1d24a6812b3a79",
+  "index.html": "12aba3c4f6469d2bf0f4e91b398506f3",
+"/": "12aba3c4f6469d2bf0f4e91b398506f3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/AssetManifest.json": "57d68a4a2737d2178f22f5afd5c65eb1",
 "assets/NOTICES": "71002b94e3941f48ee2d862c88cd9c9d",
@@ -27,7 +27,7 @@ const RESOURCES = {
 "assets/FontManifest.json": "30f3eb878c995a5bf9697e9da4ff2a45",
 "assets/shaders/ink_sparkle.frag": "940b325aa158e0f692daf36e0ae95220",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"main.dart.js": "1f8246f09bbb7974ae618bcbd3788a6e",
+"main.dart.js": "51688a50182986424d813d6ad39d9afd",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
